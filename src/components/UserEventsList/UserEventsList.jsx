9d17a41b3c152386783b6c771ebfeeaedb154554
Rajat <razat249@gmail.com>
2017-03-28 20:10:25 +0530

    add comments to timeline - need improvement

--- src/components/UserEventsList/UserEventsList.jsx
+++ src/components/UserEventsList/UserEventsList.jsx
@@ -12,22 +12,37 @@ class UserEventsList extends Component {
     generateTimeline(eventList) {
         console.log(eventList)
         var timelineList = eventList.map(function (event) {
+            const github_base_url = 'https://github.com/';
             let eventData = {
                 title: "",
-                dateTime: "",
+                dateTime: <Timestamp time={event.created_at} format='full' />,
                 icon: "",
                 data: ""
             };
             switch (event.type) {
                 case "PushEvent": {
-                    eventData.title = (<p>Pushed {event.payload.size} commit(s) to
-                                            <a href={event.repo.name}> {event.repo.name}</a>
+                    eventData.title = (<p><b>Pushed {event.payload.size} commit(s) to
+                                            <a href={github_base_url + event.repo.name}> {event.repo.name}</a></b>
                                       </p>);
-                    eventData.dateTime = <Timestamp time={event.created_at} format='full' />;
                     eventData.icon = <Icon name="link" />;
                     eventData.data = event.payload.commits.map(function (commit) {
-                        return <p>{commit.message}</p>
+                        return <p key={commit.sha}><strong>{commit.sha.slice(0,5)}</strong> - { commit.message }</p>
                     });
+                    break;
+                }
+                case "IssueCommentEvent": {
+                    eventData.title = (
+                        <p><b>
+                            Commented on an <a href={event.payload.issue.html_url}>Issue</a> on 
+                            <a href={github_base_url + event.repo.name}> {event.repo.name}</a>
+                        </b></p>
+                    );
+                    eventData.data = (
+                        <blockquote>
+                            {event.payload.issue.body}
+                        </blockquote>
+                    )
+                    break;
                 }
             }
             return (
