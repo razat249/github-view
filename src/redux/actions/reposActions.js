183548e475968981e43e32819f6f0c0a6893928f
Rajat <razat249@gmail.com>
2017-03-29 22:42:13 +0530

    build for running on github pages

--- src/redux/actions/reposActions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function fetchUserRepos(user) {
-    return {
-        type: "FETCH_REPOS", 
-        payload: axios.get(`https://api.github.com/users/${user}/repos?sort=pushed`)
-    }
-}
--- src/redux/actions/usersActions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function fetchUsers(query) {
-    return {
-        type: "FETCH_USERS", 
-        payload: axios.get('https://api.github.com/search/users?q=' + query)
-    }
-}
